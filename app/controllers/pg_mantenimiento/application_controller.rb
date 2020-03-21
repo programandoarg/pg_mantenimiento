532e93f583ffabe3d947bf629f321bd7088c1158
Martín Rosso <mrosso10@gmail.com>
2020-03-21 02:42:07 -0300

    authenticate

--- app/controllers/pg_mantenimiento/application_controller.rb
+++ app/controllers/pg_mantenimiento/application_controller.rb
@@ -2,6 +2,9 @@ module PgMantenimiento
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
+    # esto es medio choto, porque obliga a tener devise pero bueno
+    before_action :authenticate_user!
+
     before_action do
       @empresa = PgMantenimiento.config.nombre_empresa
     end
