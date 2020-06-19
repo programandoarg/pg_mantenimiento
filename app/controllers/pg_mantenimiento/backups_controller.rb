56653e64ef566cdac56fc856b658333dd30dc8a0
Martín Rosso <mrosso10@gmail.com>
2020-06-19 11:07:12 -0300

    arreglos en el GET de los backups

--- app/controllers/pg_mantenimiento/backups_controller.rb
+++ app/controllers/pg_mantenimiento/backups_controller.rb
@@ -7,24 +7,31 @@ module PgMantenimiento
     end
 
     def show
-      @archivo = cliente_s3.get_object(bucket: PgMantenimiento.config.aws_s3[:bucket], key: params[:key])
+      @archivo = cliente_s3.head_object(bucket: PgMantenimiento.config.aws_s3[:bucket], key: params[:key])
       @key = params[:key]
     rescue Aws::S3::Errors::NoSuchKey => e
-      flash[:error] = "el archivo no existe"
+      flash[:error] = "El archivo no existe"
+      redirect_back fallback_location: backups_index_path
+    rescue Aws::S3::Errors::InvalidObjectState => e
+      flash[:error] = "No se puede acceder al archivo"
       redirect_back fallback_location: backups_index_path
     end
 
     def descargar
-      @archivo = cliente_s3.get_object(bucket: PgMantenimiento.config.aws_s3[:bucket], key: params[:key])
+      @archivo = cliente_s3.head_object(bucket: PgMantenimiento.config.aws_s3[:bucket], key: params[:key])
       maximos_megas = 50
       if @archivo.content_length < (maximos_megas * 1024 * 1024) # si supera el tamaño permitido
+        @archivo = cliente_s3.get_object(bucket: PgMantenimiento.config.aws_s3[:bucket], key: params[:key])
         send_data @archivo.body.read, filename: nombre_archivo(params[:key])
       else
-        @error = "no se puede descargar el archivo"
+        flash[:error] = "No se puede descargar el archivo"
         redirect_back fallback_location: backups_index_path
       end
     rescue Aws::S3::Errors::NoSuchKey => e
-      @error = "el archivo no existe"
+      flash[:error] = "El archivo no existe"
+      redirect_back fallback_location: backups_index_path
+    rescue Aws::S3::Errors::InvalidObjectState => e
+      flash[:error] = "No se puede acceder al archivo"
       redirect_back fallback_location: backups_index_path
     end
   end
